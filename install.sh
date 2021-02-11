e48fb64b4b820510028739f680b26657e485f593
wusung <twkoci@gmail.com>
2021-02-11 11:40:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 
-```
 brew install openssl
-```
-
-### Add to path
-```
 brew install php@7.4
-```
+# Add to path
 
-### 安裝 swoole
-選項依續選 No, Yes, Yes, Yes, No
+# 安裝 swoole
+# 選項依續選 No, Yes, Yes, Yes, No
 
-
-### 安裝 php redis
-
-```
+# 安裝 php redis
 pecl install swoole
-```
 
-### 安裝 php redis
-
-```
-pecl install swoole
-```
+# 安裝 php redis
+pecl install redis
